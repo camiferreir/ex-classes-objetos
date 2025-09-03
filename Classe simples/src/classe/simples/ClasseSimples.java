a65a6138a75e28dbc191f61de9b452d576b5c8a4
Camille <ferreiracamille819@gmail.com>
2025-09-03 14:52:22 -0300

    Add files via upload

---
+++ Classe simples/src/classe/simples/ClasseSimples.java
@@ -0,0 +1,15 @@
+
+package classe.simples;
+
+
+public class ClasseSimples {
+
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa();
+        p1.nome = "Camille";
+        p1.idade = 20;
+        p1.profissao = "Estudante";
+        p1.exibirDetalhes();
+    }
+    
+}
---
+++ Classe simples/src/classe/simples/Pessoa.java
@@ -0,0 +1,14 @@
+
+package classe.simples;
+
+class Pessoa {
+      String nome;
+    int idade;
+    String profissao;
+    
+    void exibirDetalhes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Profissão: " + profissao);
+    }
+}
