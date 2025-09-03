ea3bbe6855ac4edc8d0b15e193b5c4e71a97bddd
Camille <ferreiracamille819@gmail.com>
2025-09-03 14:56:21 -0300

    Add files via upload

---
+++ Contador/src/contador/Contador.java
@@ -0,0 +1,24 @@
+package contador;
+
+public class Contador {
+
+    public static void main(String[] args) {
+        Contador contador = new Contador();
+        contador.incrementar();
+        contador.incrementar();
+        contador.decrementar();
+        contador.exibirValor();
+    }
+
+    private void incrementar() {
+
+    }
+
+    private void decrementar() {
+
+    }
+
+    private void exibirValor() {
+
+    }
+}
---
+++ Contador/src/contador/contador1.java
@@ -0,0 +1,18 @@
+package contador;
+
+public class contador1 {
+
+    int valor = 0;
+
+    void incrementar() {
+        valor++;
+    }
+
+    void decrementar() {
+        valor--;
+    }
+
+    void exibirValor() {
+        System.out.println("Valor atual do contador: " + valor);
+    }
+}
