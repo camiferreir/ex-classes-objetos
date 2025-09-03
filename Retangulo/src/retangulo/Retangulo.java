3c07f8ed372b8850dacffea0a1779d09a8238bca
Camille <ferreiracamille819@gmail.com>
2025-09-03 14:55:51 -0300

    Add files via upload

---
+++ Retangulo/src/retangulo/Retangulo.java
@@ -0,0 +1,28 @@
+
+package retangulo;
+
+
+public class Retangulo {
+    
+    public static void main(String[] args) {
+        Retangulo ret = new Retangulo();
+        ret.largura = 5;
+        ret.altura = 3;
+
+        System.out.println("Área: " + ret.calcularArea());
+        System.out.println("Perímetro: " + ret.calcularPerimetro());
+    }
+    private int largura;
+    private int altura;
+
+    private String calcularArea() {
+        return null;
+      
+    }
+
+    private String calcularPerimetro() {
+        return null;
+      
+    }
+}
+    
---
+++ Retangulo/src/retangulo/retan.java
@@ -0,0 +1,15 @@
+package retangulo;
+
+public class retan {
+
+    double largura;
+    double altura;
+
+    double calcularArea() {
+        return largura * altura;
+    }
+
+    double calcularPerimetro() {
+        return 2 * (largura + altura);
+    }
+}
